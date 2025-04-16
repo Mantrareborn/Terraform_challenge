437d1a52da6bc41a2cc62de0421d4b01b781a20d
Dali Hendaoui <dali_hendaoui@hotmail.com>
2025-04-16 15:22:39 +0100

    remove everything

--- environments/dev/output.tf
+++ environments/dev/output.tf
@@ -1,7 +1,7 @@
-output "vnet_id" {
-  value = module.vnet.vnet_id
-}
+# output "vnet_id" {
+#   value = module.vnet.vnet_id
+# }
 
-output "subnet_id" {
-  value = module.vnet.subnet_id
-}
+# output "subnet_id" {
+#   value = module.vnet.subnet_id
+# }
