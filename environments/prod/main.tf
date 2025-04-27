a9c0d310557db725b8d8830246f96a20e9626479
Dali Hendaoui <dali_hendaoui@hotmail.com>
2025-04-27 12:52:49 +0100

    remove resources

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,17 +1,17 @@
-# resource "azurerm_resource_group" "rg" {
-#   name     = "rg-${var.environment}-${var.location}"
-#   location = var.location
-#   tags     = var.tags
-# }
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${var.environment}-${var.location}"
+  location = var.location
+  tags     = var.tags
+}
 
-# module "vnet" {
-#   source              = "../../modules/vnet"
+module "vnet" {
+  source              = "../../modules/vnet"
 
-#   vnet_name       = "vnet-${var.environment}-${var.location}"
-#   resource_group_name       = azurerm_resource_group.rg.name
-#   location                  = var.location
-#   address_space             = var.vnet_address_space
-#   subnet_name               = var.subnet_name
-#   subnet_address_prefixes   = var.subnet_address_prefixes
-#   tags                      = var.tags
-# }
+  vnet_name       = "vnet-${var.environment}-${var.location}"
+  resource_group_name       = azurerm_resource_group.rg.name
+  location                  = var.location
+  address_space             = var.vnet_address_space
+  subnet_name               = var.subnet_name
+  subnet_address_prefixes   = var.subnet_address_prefixes
+  tags                      = var.tags
+}
